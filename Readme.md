dae6a61b01224ee083d8919a59def473fa2509ad
Leandro <eandro.frigerio@devugees.org>
2018-01-24 13:07:14 +0100

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,28 @@
+# Students Application
+
+Restful Service with Node, Express, Bootstrap and MongoDB for Devugees
+
+## Requirements
+
+You need to have mongo Up&Running. You can configure the db from config/database.config.js
+An export of the
+
+
+## Setup
+
+1. Install dependencies
+
+```bash
+npm install
+```
+
+2. Run Server
+
+```bash
+npm start
+```
+
+You can browse the apis at <http://localhost:3000>
+
+
+
---
+++ db/export.js
@@ -0,0 +1,29 @@
+/* 1 */
+{
+    "_id" : ObjectId("5a676ed17d5d1177cefc0f23"),
+    "subjects" : [ 
+        "math", 
+        "sport", 
+        "history"
+    ],
+    "name" : "chris",
+    "age" : 30,
+    "gender" : "male",
+    "updated" : ISODate("2018-01-23T17:20:17.205Z"),
+    "__v" : 0
+}
+
+/* 2 */
+{
+    "_id" : ObjectId("5a67722a0376d377f81005e2"),
+    "subjects" : [ 
+        "physics", 
+        "sport", 
+        "geography"
+    ],
+    "name" : "sarah",
+    "age" : 23,
+    "gender" : "female",
+    "updated" : ISODate("2018-01-23T17:34:34.659Z"),
+    "__v" : 0
+}
